a61b67cfededfff274450d919d702d2d32942ca4
Étienne Gauvin <etienne@gauvin.info>
2022-07-09 17:48:16 +0200

    Added the description of the `generate` command

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,49 @@ use std::{collections::HashMap};
 use std::path::PathBuf;
 use colored::*;
 use std::fs;
-use serde_derive::{Deserialize};
-use clap::{Parser};
+use clap::{Parser, Subcommand};
 
-#[derive(Parser, Debug)]
+const DEFAULT_RENAMINGS_FILE: &str = "_renamings.yml";
+
+
+#[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Input file
+    /// YAML file listing all the paths to rename.
+    /// 
+    /// Each line of the file must be a pair `key: value`,
+    /// where the key is the path to rename from, and the value is the path to rename to.
+    /// Defaults to "_renamings.yml".
     #[clap(parse(from_os_str))]
-    input_file: PathBuf,
+    input_renamings_file: Option<PathBuf>,
 
-    /// Do a dry run
-    #[clap(long, action, default_value_t = false)]
+    /// Just verify that each renaming is possible but do not actually rename anything.
+    #[clap(short, long, action, default_value_t = false)]
     dry_run: bool,
 
-    /// Revert changes
+    /// Read each renaming from the input file in reverse, so the value becomes the path to rename from,
+    /// and the key becomes the path to rename to.
     #[clap(long, action, default_value_t = false)]
     revert: bool,
 
+    #[clap(subcommand)]
+    commands: Option<Commands>,
 }
 
-#[derive(Debug, Deserialize)]
-struct InputFileFormat {
-    rename: HashMap<String, String>,
+#[derive(Subcommand)]
+enum Commands {
+    /// Generates a YAML file listing all the files in a directory.
+    Generate {
+        /// Path of the files to list.
+        /// Defaults to the current directory.
+        #[clap(parse(from_os_str))]
+        from_path: Option<PathBuf>,
+
+        /// YAML file into which write the files list.
+        /// Defaults to "_renamings.yml".
+        #[clap(parse(from_os_str))]
+        output_renamings_file: Option<PathBuf>,
+    },
 }
 
 enum InputPathError {
@@ -39,30 +59,58 @@ enum OutputPathError {
 fn main() {
     let args = Args::parse();
 
-    let config_file = match std::fs::File::open(args.input_file) {
+    match args.commands {
+        Some(command) => match command {
+            Commands::Generate {output_renamings_file: renamings_file, from_path} => {
+                generate_command(
+                    renamings_file.unwrap_or(PathBuf::from(DEFAULT_RENAMINGS_FILE)),
+                    from_path.unwrap_or(PathBuf::from(".")),
+                )
+            }
+        },
+
+        // Default command
+        None => {
+            rename_command(
+                args.input_renamings_file.unwrap_or(PathBuf::from(DEFAULT_RENAMINGS_FILE)),
+                args.dry_run,
+                args.revert
+            )
+        },
+    };
+
+}
+
+fn generate_command(renamings_file: PathBuf, from_path: PathBuf) {
+    println!("Gen {:?} from {:?}", renamings_file, from_path)
+}
+
+fn rename_command(renamings_file: PathBuf, dry_run: bool, revert: bool) {
+
+    let renamings_file = match std::fs::File::open(renamings_file) {
         Ok(file) => file,
         Err(error) => panic!("{}", format!("Problem opening the file {:?}", error).red()),
     };
 
-    let config: InputFileFormat = match serde_yaml::from_reader(config_file) {
-        Ok(config) => config,
+    let renamings: HashMap<String, String> = match serde_yaml::from_reader(renamings_file) {
+        Ok(renamings) => renamings,
         Err(error) => panic!("{}", format!("Problem parsing the YAML: {:?}", error).red()),
     };
 
-    for (mut input, mut output) in &config.rename {
-        if args.revert {
+    for (mut input, mut output) in &renamings {
+        if revert {
             (input, output) = (output, input);
         }
 
         let result_input_path = verify_input_path(&input);
-        let result_output_path = verify_output_path(&output, &config.rename);
+        let result_output_path = verify_output_path(&output, &renamings);
 
 
         match result_input_path {
             Ok(()) => println!("{}", format!("  from: {:?}", &input)),
 
             Err(InputPathError::PathDoesntExists) => {
-                println!("{}", format!("✗ from: {:?} this path doesn't exist!", &input).red().bold());
+                println!("{}", format!("✗ from: {:?} this path doesn't exist!", &input).red());
             },
         }
 
@@ -70,23 +118,23 @@ fn main() {
             Ok(()) => println!("{}", format!("    to: {:?}", &output)),
 
             Err(OutputPathError::PathUsedSeveralTimes(count)) => {
-                println!("{}", format!("✗   to: {:?} is used {} times, but it should be unique!", &output, count).red().bold());
+                println!("{}", format!("✗   to: {:?} is used {} times, but it should be unique!", &output, count).red());
             },
 
             Err(OutputPathError::PathAlreadyExists) => {
-                println!("{}", format!("✗   to: {:?} this path already exist!", &output).red().bold());
+                println!("{}", format!("✗   to: {:?} this path already exist!", &output).red());
             },
         }
 
         if matches!(result_input_path, Err(InputPathError::PathDoesntExists)) && matches!(result_output_path, Err(OutputPathError::PathAlreadyExists)) {
-            println!("{}", format!("⚠ it seems this path was already renamed.").blue().bold());
+            println!("{}", format!("⚠ it seems this path was already renamed.").blue());
         }
 
         if matches!(result_input_path, Ok(())) && matches!(result_output_path, Ok(())) {
-            if !args.dry_run {
+            if !dry_run {
                 match fs::rename(input, output) {
-                    Ok(()) => println!("{}", "✓ correctly renamed!".green().bold()),
-                    Err(_) => println!("{}", "✗ could not be rename!".red().bold())
+                    Ok(()) => println!("{}", "✓ correctly renamed!".green()),
+                    Err(_) => println!("{}", "✗ could not be renamed!".red())
                 }
             }
         }
@@ -94,7 +142,7 @@ fn main() {
         println!();
     }
 
-    if args.dry_run {
+    if dry_run {
         println!("{}", "Currently in dry run, so no file have been modified.".blue().bold().underline());
         return
     }
